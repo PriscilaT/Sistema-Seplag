d1f039fbe962035e4f55b4e060bea9c52fb5d43f
Fernando Lins <fernandolins088@gmail.com>
2021-08-06 11:51:39 -0300

    Update edit views

--- resources/views/funcionarios/editar.blade.php
+++ resources/views/funcionarios/editar.blade.php
@@ -12,126 +12,116 @@
 
             <form action="{{ route('funcionario.update', $funcionario->idFuncionario) }}" method="POST">
                 @method('PATCH')
-
-                <div class="form-group row">
-                    <label class="col-sm-3 col-form-label" for="idFuncionario">
-                        ID do Funcionário:
-                    </label>
-                    <input class="col-sm-9" type="text" name="idFuncionario">
-                </div>
                 
                 <div class="form-group row">
                     <label class="col-sm-3 col-form-label" for="nome">
                         Nome do Funcionário:
                     </label>
-                    <input class="col-sm-9" type="text" name="nome">
+                    <input value="<?= $funcionario->nome ?>" class="col-sm-9" type="text" name="nome">
                 </div>
 
                 <div class="form-group row">
                     <label class="col-sm-3 col-form-label" for="rg">
                         rg:
                     </label>
-                    <input class="col-sm-9" type="text" name="rg">
+                    <input value="<?= $funcionario->rg ?>" class="col-sm-9" type="text" name="rg">
                 </div>
 
                 <div class="form-group row">
                     <label class="col-sm-3 col-form-label" for="cpf">
                         cpf:
                     </label>
-                    <input class="col-sm-9" type="text" name="cpf">
+                    <input value="<?= $funcionario->cpf ?>" class="col-sm-9" type="text" name="cpf">
                 </div>
 
                 <div class="form-group row">
                     <label class="col-sm-3 col-form-label" for=" nascimento">
                         nascimento:
                     </label>
-                    <input class="col-sm-9" type="text" name=" nascimento">
+
+                    <input value="<?= $funcionario->nascimento ?>" class="col-sm-9" type="text" name=" nascimento">
                 </div>
 
                 <div class="form-group row">
                     <label class="col-sm-3 col-form-label" for="genero">
                         genero:
                     </label>
-                    <input class="col-sm-9" type="text" name="genero">
+                    <input value="<?= $funcionario->genero ?>" class="col-sm-9" type="text" name="genero">
                 </div>
 
                 <div class="form-group row">
                     <label class="col-sm-3 col-form-label" for="estado_civil">
                         estado_civil:
                     </label>
-                    <input class="col-sm-9" type="text" name="estado_civil">
+                    <?php 
+                        $estadoCivil = $funcionario->estado_civil;
+                        ucfirst($estadoCivil);
+                    ?>
+                    <input value="<?= $estadoCivil ?>" class="col-sm-9" type="text" name="estado_civil">
                 </div>
 
                 <div class="form-group row">
                     <label class="col-sm-3 col-form-label" for="email">
                         email:
                     </label>
-                    <input class="col-sm-9" type="text" name="email">
+                    <input value="<?= $funcionario->email ?>" class="col-sm-9" type="text" name="email">
                 </div>
 
                 <div class="form-group row">
                     <label class="col-sm-3 col-form-label" for="telefone">
                         telefone:
                     </label>
-                    <input class="col-sm-9" type="text" name="telefone">
+                    <input value="<?= $funcionario->telefone ?>" class="col-sm-9" type="text" name="telefone">
                 </div>
 
                 <div class="form-group row">
                     <label class="col-sm-3 col-form-label" for="cep">
                         cep:
                     </label>
-                    <input class="col-sm-9" type="text" name="cep">
+                    <input value="<?= $funcionario->cep ?>" class="col-sm-9" type="text" name="cep">
                 </div>
 
                 <div class="form-group row">
                     <label class="col-sm-3 col-form-label" for="rua">
                         rua:
                     </label>
-                    <input class="col-sm-9" type="text" name="rua">
+                    <input value="<?= $funcionario->rua ?>" class="col-sm-9" type="text" name="rua">
                 </div>
 
                 <div class="form-group row">
                     <label class="col-sm-3 col-form-label" for="numero">
                         numero:
                     </label>
-                    <input class="col-sm-9" type="text" name="numero">
+                    <input value="<?= $funcionario->numero ?>" class="col-sm-9" type="text" name="numero">
                 </div>
 
                 <div class="form-group row">
                     <label class="col-sm-3 col-form-label" for="bairro">
                         bairro:
                     </label>
-                    <input class="col-sm-9" type="text" name="bairro">
+                    <input value="<?= $funcionario->bairro ?>" class="col-sm-9" type="text" name="bairro">
                 </div>
 
                 <div class="form-group row">
                     <label class="col-sm-3 col-form-label" for="cidade">
                         cidade:
                     </label>
-                    <input class="col-sm-9" type="text" name="cidade">
+                    <input value="<?= $funcionario->cidade ?>" class="col-sm-9" type="text" name="cidade">
                 </div>
 
                 <div class="form-group row">
                     <label class="col-sm-3 col-form-label" for="estado">
                         estado:
                     </label>
-                    <input class="col-sm-9" type="text" name="estado">
+                    <input value="<?= $funcionario->estado ?>" class="col-sm-9" type="text" name="estado">
                 </div>
 
                 <div class="form-group row">
                     <label class="col-sm-3 col-form-label" for="complemento">
                         complemento:
                     </label>
-                    <textarea class="col-sm-9" name="complemento" rows="4"></textarea>
+                    <textarea class="col-sm-9" name="complemento" rows="4"><?= $funcionario->complemento ?></textarea>
                 </div>
-
-                <div class="form-group row">
-                    <label class="col-sm-3 col-form-label" for="senha">
-                        senha:
-                    </label>
-                    <input class="col-sm-9" type="text" name="senha">
-                </div>
-                
 
                 <div class="form-group row">
                     <div class="col-sm-10">
