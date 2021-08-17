4a6c940284bb05bab3839e4716dbbc7ece74e3ef
priscilat <priscila-teodorio@hotmail.com>
2021-08-17 07:19:01 -0300

    fix welcome screen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,21 +6,29 @@
         <div class="col-md-8">
         
             <div class="card">
-                <div class="card-header">{{ __('Página inicial') }}</div>
+                <div class="card-header">{{ __('Página do ADMIN') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                    <div class="content">
+                    <div class="title m-b-md">
+                        Sistema SEPLAG 
+                    </div>
 
-                    {{ __('Você está logado!') }}
 
-                    <br>
-
-                    <a href="/funcionario">Funcionário </a>
-                
+                    <div class="links">
+                        <a href="/funcionario">Funcionários </a><br>
+                        <a href="/curso">Cursos </a><br>
+                        <a href="/instituicao">Instituições </a><br>
+                        <a href="/cargo">Cargos </a><br>
+                        <a href="/tecnologia">Tecnologias </a><br>
+                        <a href="/projeto">Projetos </a><br>
+                        
+                    </div>
                 </div>
             
             </div> 
